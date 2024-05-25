cb1e3306642ca43ed953961b6c57ad2f217718f8
Harsh48 <harsh.techilia@gmail.com>
2024-05-25 08:47:37 +0530

    fix: npm version

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,9 @@ require("@nomiclabs/hardhat-ethers");
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    testnet: {
-      url: `https://sepolia.infura.io/v3/5982800e8b2940c689c2b7335f104c61`,
-      accounts: [`0x6596c4628d692bf04df6f9de254090db6d7f80f80ea55bd5f167f13d844e7f04`]
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/5982800e8b2940c689c2b7335f104c61`,
+      accounts: [``]
     }
   }
 };
