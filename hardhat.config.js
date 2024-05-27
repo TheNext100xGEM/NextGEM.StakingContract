f7a30545802af4b421e55d55393f27117cd2d50e
Harsh48 <harsh.techilia@gmail.com>
2024-05-27 07:12:48 +0530

    fix: issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     mainnet: {
       url: `https://sepolia.infura.io/v3/5982800e8b2940c689c2b7335f104c61`,
-      accounts: [`0x6596c4628d692bf04df6f9de254090db6d7f80f80ea55bd5f167f13d844e7f04`]
+      accounts: [``]
     }
   }
 };
